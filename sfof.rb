16443ef403f8a8fa2ca348a34d612df979414edb
Samuel Farrens <samuel.farrens@gmail.com>
2020-05-31 15:52:36 +0200

    Update sfof.rb

--- sfof.rb
+++ sfof.rb
@@ -4,6 +4,7 @@ class Sfof < Formula
   desc "Super Friends-of-Friends (SFoF): Galaxy Cluster Detection Algorithm"
   homepage "https://github.com/sfarrens/sfof"
   url "https://github.com/sfarrens/sfof/archive/v4.0_beta.tar.gz"
+  sha256 "145a47cd45a0dcde3bc3a32304e13a9460f8eff0c74f7988c6bc9bfd9aa99be5"
 
   # List dependencies
   depends_on "cmake" => :build
@@ -13,9 +14,7 @@ class Sfof < Formula
 
   def install
     
-    system "mkdir build"
-    system "cd build"
-    system "cmake", "..", *std_cmake_args
+    system "cmake", ".", *std_cmake_args
     system "make"
     system "make install"
 
