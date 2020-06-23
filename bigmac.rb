1ec745c353d39de8542f1c64e08bef2ca99dddc6
Samuel Farrens <samuel.farrens@gmail.com>
2020-06-23 10:32:13 +0200

    Update bigmac.rb

--- bigmac.rb
+++ bigmac.rb
@@ -4,7 +4,7 @@ class Bigmac < Formula
   desc "macOS configuration parameters"
   homepage "https://github.com/sfarrens/BigMac"
   url "https://github.com/sfarrens/BigMac/archive/v0.0.2_beta.tar.gz"
-  sha256 ""
+  sha256 "caacbe91e276209fcac0b41d57789c2b05e481509af4772676cb30f662142d3e"
 
   # List dependencies
   depends_on "libomp"
