6ed260638e1e9ad797dc40c1a6897b01dfeedfb9
Samuel Farrens <samuel.farrens@gmail.com>
2020-08-04 14:50:46 +0200

    Update bigmac.rb

--- bigmac.rb
+++ bigmac.rb
@@ -3,7 +3,7 @@
 class Bigmac < Formula
   desc "macOS configuration parameters"
   homepage "https://github.com/sfarrens/BigMac"
-  url "https://github.com/sfarrens/BigMac/archive/v0.0.3_beta.tar.gz"
+  url "https://github.com/sfarrens/BigMac/archive/v0.0.4_beta.tar.gz"
   sha256 ""
 
   # List dependencies
