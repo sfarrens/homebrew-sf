fe537dff4d1db440a8d8505356deeac7846b0dd8
Samuel Farrens <samuel.farrens@gmail.com>
2020-08-05 11:10:03 +0200

    Update bigmac.rb

--- bigmac.rb
+++ bigmac.rb
@@ -4,13 +4,13 @@ class Bigmac < Formula
   desc "macOS configuration parameters"
   homepage "https://github.com/sfarrens/BigMac"
   url "https://github.com/sfarrens/BigMac/archive/v0.0.4_beta.tar.gz"
-  sha256 ""
+  sha256 "27abb50b849ace439a3c8991cde20be1b89b4fdd32401252d9ea939622ca2366"
 
   # List dependencies
   depends_on "libomp"
 
   def install
-    system "cmake ."
+    system "cmake", "."
     system "make"
     system "make install"
   end
