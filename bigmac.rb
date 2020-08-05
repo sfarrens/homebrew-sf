852206396483fef62176e4b76a4348added78671
Samuel Farrens <samuel.farrens@gmail.com>
2020-08-05 11:59:04 +0200

    Update bigmac.rb

--- bigmac.rb
+++ bigmac.rb
@@ -7,6 +7,7 @@ class Bigmac < Formula
   sha256 "9c52eec49a40a3c972cf511a591ee38498e8657a97b50c8437c5c9192e445670"
 
   # List dependencies
+  depends_on "cmake" => :build
   depends_on "libomp"
 
   def install
