735d1c899a6fd2b72fb6f190daabbab62a8ed9cf
Samuel Farrens <samuel.farrens@gmail.com>
2020-08-06 14:36:11 +0200

    Update bigmac.rb

--- bigmac.rb
+++ bigmac.rb
@@ -3,8 +3,8 @@
 class Bigmac < Formula
   desc "macOS configuration parameters"
   homepage "https://github.com/sfarrens/bigmac"
-  url "https://github.com/sfarrens/bigmac/archive/v0.0.5_beta.tar.gz"
-  sha256 "9c52eec49a40a3c972cf511a591ee38498e8657a97b50c8437c5c9192e445670"
+  url "https://github.com/sfarrens/bigmac/archive/v0.0.6_beta.tar.gz"
+  sha256 ""
 
   # List dependencies
   depends_on "cmake" => :build
@@ -12,7 +12,6 @@ class Bigmac < Formula
 
   def install
     system "cmake", ".", *std_cmake_args
-    system "make"
     system "make install"
   end
 end
