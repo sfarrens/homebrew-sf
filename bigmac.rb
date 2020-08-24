a683188fb340ea373f724d53fe1956977a0d0ec0
Samuel Farrens <samuel.farrens@gmail.com>
2020-08-24 16:55:18 +0200

    Update bigmac.rb

--- bigmac.rb
+++ bigmac.rb
@@ -5,6 +5,7 @@ class Bigmac < Formula
   homepage "https://github.com/sfarrens/bigmac"
   url "https://github.com/sfarrens/bigmac/archive/v0.0.7_beta.tar.gz"
   sha256 "40cceb3a79f012876a1b2041112eaf31b1cd323e4470c1e7a0f541f7974ada47"
+  license "MIT"
 
   # List dependencies
   depends_on "cmake" => :build
